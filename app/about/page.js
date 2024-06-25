fe8d0864ac915dc44ddd400004947fc7142aa9fc
mycallCampbell <mycallcampbell@gmail.com>
2024-06-25 23:10:03 +0100

    updated about us page with text

--- app/about/page.js
+++ app/about/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       {/* MOBILE HERO */}
       <div className={styles.containerAboutUs}>
         <Image
-          className={styles.aboutUs}
+          className={styles.heroImageMobile}
           src={'/about_us1.jpg'}
           width={1456}
           height={816}
@@ -27,8 +27,22 @@ export default function Home() {
      
       <div className={styles.introText}>Hartcrown Limited have now been established for 35 years within the London region and surrounding areas, carrying out all aspects of Groundworks and Civil Engineering projects together with 278 works and diversionary road schemes for our core clients with repeat business as our main principal aim and establishing good working relationships. </div>
 
+      <div className={styles.introText}>
+       Contract values that we would be happy to undertake range from £150,000.00 to £2,500,000.00
+      </div>
 
-      
+      <Image
+        className={styles.heroImageMobile}
+        src={'/about_us.jpg'}
+        width={1456}
+        height={816}
+        alt={'hero image'}
+        quality={100}
+      />
+
+      <div className={styles.introText}>
+       Most of our work is negotiated with a selective list of clients that have stayed with us for many years, having built up a good relationship with both senior and site management teams, and we actively assist them both at tender stage through to completion of the contact. Our management team have excellent experience gained over the years, whereby we are able to get involved at an early tender stage to offer value engineering solutions where necessary.
+      </div>
 
       <div className={styles.contactBTNContainer}>
           <Link href="/" className={styles.linkBTN}>Contact</Link>
